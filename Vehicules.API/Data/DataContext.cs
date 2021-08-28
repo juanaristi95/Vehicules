2de5d5da31e3547165477a35c778923a8c5b39a7
juanaristi95 <46731700+juanaristi95@users.noreply.github.com>
2021-08-28 15:09:52 -0500

    arquitecture of project part 1

---
+++ Vehicules.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicules.API.Data.Entities;
+
+namespace Vehicules.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<VehicleType> VehiclesTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
