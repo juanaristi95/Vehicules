65c74d72b8bc5275a518edd3a3c132c5fda41609
juanaristi95 <46731700+juanaristi95@users.noreply.github.com>
2021-09-11 15:49:13 -0500

    update and delete user

--- Vehicules.API/Models/UserViewModel.cs
+++ Vehicules.API/Models/UserViewModel.cs
@@ -54,5 +54,10 @@ namespace Vehicules.API.Models
         public int DocumentTypeId { get; set; }
 
         public IEnumerable<SelectListItem> DocumentTypes { get; set; }
+
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44317/images/noimage.png"
+            : $"https://vehiclesjda.blob.core.windows.net/users/{ImageId}";
     }
 }
