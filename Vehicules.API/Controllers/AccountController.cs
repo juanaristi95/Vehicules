a8446d056948face73c171533ce8f3e44eb265b4
juanaristi95 <46731700+juanaristi95@users.noreply.github.com>
2021-09-12 11:26:38 -0500

    Selft registration user

--- Vehicules.API/Controllers/AccountController.cs
+++ Vehicules.API/Controllers/AccountController.cs
@@ -2,18 +2,27 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Vehicules.API.Data;
+using Vehicules.API.Data.Entities;
 using Vehicules.API.Helpers;
 using Vehicules.API.Models;
+using Vehicules.Common.Enums;
 
 namespace Vehicles.API.Controllers
 {
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
+        private readonly DataContext _context;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public AccountController(IUserHelper userHelper)
+        public AccountController(IUserHelper userHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper)
         {
             _userHelper = userHelper;
+            _context = context;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
         }
 
         public IActionResult Login()
@@ -57,6 +66,56 @@ namespace Vehicles.API.Controllers
         public IActionResult NotAuthorized()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            AddUserViewModel model = new AddUserViewModel
+            {
+                DocumentTypes = _combosHelper.GetCombosDocumentTypes()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _userHelper.AddUserAsync(model, imageId, UserType.User);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado por otro usuario.");
+                    model.DocumentTypes = _combosHelper.GetCombosDocumentTypes();
+                    return View(model);
+                }
+
+                LoginViewModel loginViewModel = new LoginViewModel
+                {
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
+
+                var result2 = await _userHelper.LoginAsync(loginViewModel);
+
+                if (result2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            model.DocumentTypes = _combosHelper.GetCombosDocumentTypes();
+            return View(model);
         }
     }
 }
